54f11efa98afe57e6c29a3267d161d753d007467
luowenkang <505515031@qq.com>
2017-07-17 16:01:05 +0800

    update libs version
    Change-Id: I1e318dae79761ee58f1009643b72d940f7d2ed82

--- ImagePicker/src/main/java/com/lwkandroid/imagepicker/utils/GlideImagePickerDisplayer.java
+++ ImagePicker/src/main/java/com/lwkandroid/imagepicker/utils/GlideImagePickerDisplayer.java
@@ -4,33 +4,30 @@ import android.content.Context;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 /**
  * Glide实现的图片加载
  */
 public class GlideImagePickerDisplayer implements IImagePickerDisplayer
 {
-
     @Override
     public void display(Context context, String url, ImageView imageView, int maxWidth, int maxHeight)
     {
         Glide.with(context)
-                .load(url)
                 .asBitmap()
-                .override(maxWidth, maxHeight)
+                .load(url)
+                .apply(new RequestOptions().override(maxWidth, maxHeight))
                 .into(imageView);
     }
 
     @Override
     public void display(Context context, String url, ImageView imageView, int placeHolder, int errorHolder, int maxWidth, int maxHeight)
     {
-
         Glide.with(context)
-                .load(url)
                 .asBitmap()
-                .placeholder(placeHolder)
-                .error(errorHolder)
-                .override(maxWidth, maxHeight)
+                .load(url)
+                .apply(new RequestOptions().placeholder(placeHolder).error(errorHolder).override(maxWidth, maxHeight))
                 .into(imageView);
     }
 }
