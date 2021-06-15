9c0799a9e548a2340e36eeef3b5e5b7510ba1f71
luowenkang <505515031@qq.com>
2021-06-15 15:55:15 +0800

    coding

--- library/src/main/java/com/lwkandroid/imagepicker/widget/CheckView.java
+++ library/src/main/java/com/lwkandroid/imagepicker/widget/CheckView.java
@@ -2,8 +2,14 @@ package com.lwkandroid.imagepicker.widget;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -28,15 +34,19 @@ public class CheckView extends View
     //默认尺寸，48dp
     private static final int DEFAULT_SIZE = 48;
 
-    private boolean checked;
-    private int textSize;
-    private int backgroundColor;
-    private int borderColor;
-    private int borderWidth;
-    private int numberCenter;
-    private Drawable drawableCenter;
+    private boolean mChecked;
+    private int mTextSize;
+    private int mBackgroundColor;
+    private int mBorderColor;
+    private int mBorderWidth;
+    private int mNumberCenter;
+    private Drawable mDrawableCenter;
     @CheckMode
-    private int checkMode;
+    private int mCheckMode;
+
+    private Paint mBorderPaint;
+    private Paint mBackgroundPaint;
+    private TextPaint mTextPaint;
 
     @IntDef({CHECK_MODE_DRAWABLE, CHECK_MODE_NUMBER})
     @Retention(RetentionPolicy.SOURCE)
@@ -74,18 +84,68 @@ public class CheckView extends View
         setMeasuredDimension(widthSize, heightSize);
     }
 
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+        super.onDraw(canvas);
+
+    }
+
     private void init(Context context, AttributeSet attrs)
     {
-        final TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.ComActionBar);
-        checked = ta.getBoolean(R.styleable.CheckView_android_checked, false);
-        checkMode = ta.getInt(R.styleable.CheckView_checkMode, CHECK_MODE_DRAWABLE);
-        textSize = ta.getDimensionPixelOffset(R.styleable.CheckView_android_textSize,
+        final TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.CheckView);
+        mChecked = ta.getBoolean(R.styleable.CheckView_android_checked, false);
+        mCheckMode = ta.getInt(R.styleable.CheckView_checkMode, CHECK_MODE_DRAWABLE);
+        mTextSize = ta.getDimensionPixelOffset(R.styleable.CheckView_android_textSize,
                 context.getResources().getDimensionPixelOffset(R.dimen.check_view_text_size_default));
-        backgroundColor = ta.getColor(R.styleable.CheckView_android_background, Color.TRANSPARENT);
-        borderColor = ta.getColor(R.styleable.CheckView_border_color, Color.WHITE);
-        borderWidth = ta.getDimensionPixelOffset(R.styleable.CheckView_border_width,
+        mBackgroundColor = ta.getColor(R.styleable.CheckView_android_background, Color.TRANSPARENT);
+        mBorderColor = ta.getColor(R.styleable.CheckView_border_color, Color.WHITE);
+        mBorderWidth = ta.getDimensionPixelOffset(R.styleable.CheckView_border_width,
                 context.getResources().getDimensionPixelOffset(R.dimen.check_view_border_width_default));
-        numberCenter = ta.getInt(R.styleable.CheckView_numberCenter, 0);
-        drawableCenter = ta.getDrawable(R.styleable.CheckView_drawableCenter);
+        mNumberCenter = ta.getInt(R.styleable.CheckView_numberCenter, 0);
+        mDrawableCenter = ta.getDrawable(R.styleable.CheckView_drawableCenter);
+
+        initTextPaint();
+        initBorderPaint();
+        initBackgroundPaint();
+    }
+
+    private void initBorderPaint()
+    {
+        if (mBackgroundPaint != null)
+        {
+            return;
+        }
+        mBorderPaint = new Paint();
+        mBorderPaint.setAntiAlias(true);
+        mBorderPaint.setStyle(Paint.Style.STROKE);
+        mBorderPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER));
+        mBorderPaint.setStrokeWidth(mBorderWidth);
+        mBorderPaint.setColor(mBorderColor);
+    }
+
+    private void initBackgroundPaint()
+    {
+        if (mBackgroundPaint != null)
+        {
+            return;
+        }
+        mBackgroundPaint = new Paint();
+        mBackgroundPaint.setAntiAlias(true);
+        mBackgroundPaint.setStyle(Paint.Style.FILL);
+        mBackgroundPaint.setColor(mBackgroundColor);
+    }
+
+    private void initTextPaint()
+    {
+        if (mTextPaint != null)
+        {
+            return;
+        }
+        mTextPaint = new TextPaint();
+        mTextPaint.setAntiAlias(true);
+        mTextPaint.setColor(Color.WHITE);
+        mTextPaint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+        mTextPaint.setTextSize(mTextSize);
     }
 }
